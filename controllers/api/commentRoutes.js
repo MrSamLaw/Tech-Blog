85d1d3fc65509b280592dd4f868f2a2d18c7873a
Mr Sam Law <sam@chongland.com>
2021-06-10 03:07:05 +1000

    Update comments on homeRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -19,7 +19,7 @@ router.post('/', withAuth, async (req, res) => {
         ...req.body,
         user_id: req.session.user_id,
       });
-  
+  console.log(newComment);
       res.status(200).json(newComment);
     } catch (err) {
         console.log(err);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -34,6 +34,14 @@ router.get('/post/:id', async (req, res) => {
         {
           model: User,
           attributes: ['name'],
+        },
+        {
+          model: Comment,
+          attributes: ['comment', 'comment_date'],
+          include:{
+            model:User,
+            attributes: ['name'],
+          },
         },
       ],
     });
