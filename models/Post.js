825aef38b8f17a4d63e66fe74257f50448407105
Mr Sam Law <sam@chongland.com>
2021-06-09 12:07:48 +1000

    Dashboard Functionality

--- models/Post.js
+++ models/Post.js
@@ -15,7 +15,7 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
+    content: {
       type: DataTypes.STRING,
     },
     date_created: {
---
+++ public/js/dashboard.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('#post-content').value.trim();
+
+  if (name && content) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ name, content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.post-list')
+  .addEventListener('click', delButtonHandler);
